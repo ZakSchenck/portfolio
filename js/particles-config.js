3e922969543bc6329763f7415fd4a0798c0f1de1
Zak Schenck <zakschenck@zaks-mbp-2.mynetworksettings.com>
2024-05-12 00:54:50 -0400

    particles configure

--- js/particles-config.js
+++ js/particles-config.js
@@ -2,109 +2,111 @@
  * CUSTOM PARTICLES CONFIGURATION FROM PARTICLES.JS
  */
 
-particlesJS('particles-js', {
-  "particles": {
-    "number": {
-      "value": 79,
-      "density": {
-        "enable": true,
-        "value_area": 800
-      }
-    },
-    "color": {
-      "value": "#ffffff"
-    },
-    "shape": {
-      "type": "image", 
-      "image": {
-        "src": "../assets/header/brightturq.png",
-        "width": 800,
-        "height": 800
-      }
-    },
-    "opacity": {
-      "value": 0.5,
-      "random": true,
-      "anim": {
-        "enable": false,
-        "speed": 0.2,
-        "opacity_min": 0.1,
-        "sync": false
-      }
-    },
-    "size": {
-      "value": 10,
-      "random": true,
-      "anim": {
-        "enable": false,
-        "speed": 10,
-        "size_min": 0.1,
-        "sync": false
-      }
-    },
-    "line_linked": {
-      "enable": true,
-      "distance": 150,
-      "color": "#32a852",
-      "opacity": 0.4,
-      "width": 1
-    },
-    "move": {
-      "enable": true,
-      "speed": 3,
-      "direction": "none",
-      "random": false,
-      "straight": false,
-      "out_mode": "out",
-      "bounce": false,
-      "attract": {
-        "enable": false,
-        "rotateX": 600,
-        "rotateY": 1200
-      }
-    }
-  },
-  "interactivity": {
-    "detect_on": "canvas",
-    "events": {
-      "onhover": {
-        "enable": true,
-        "mode": "repulse"
-      },
-      "onclick": {
-        "enable": true,
-        "mode": "push",
-        "particles_nb": 10
-      },
-      "resize": true
-    },
-    "modes": {
-      "grab": {
-        "distance": 400,
-        "line_linked": {
-          "opacity": 1
+document.addEventListener('DOMContentLoaded', function () {
+  particlesJS('particles-js', {
+    "particles": {
+      "number": {
+        "value": 79,
+        "density": {
+          "enable": true,
+          "value_area": 800
         }
       },
-      "repulse": {
-        "distance": 200,
-        "duration": 0.4
+      "color": {
+        "value": "#ffffff"
       },
-      "bubble": {
-        "distance": 10,
-        "size": 4,
-        "duration": 2,
-        "opacity": 8,
-        "speed": 3
+      "shape": {
+        "type": "image",
+        "image": {
+          "src": "https://i.postimg.cc/SxXBfYdy/turqlight-removebg-preview.png",
+          "width": 800,
+          "height": 800
+        }
       },
-      "push": {
-        "particles_nb": 4
+      "opacity": {
+        "value": 0.5,
+        "random": true,
+        "anim": {
+          "enable": false,
+          "speed": 0.2,
+          "opacity_min": 0.1,
+          "sync": false
+        }
       },
-      "remove": {
-        "particles_nb": 2
+      "size": {
+        "value": 10,
+        "random": true,
+        "anim": {
+          "enable": false,
+          "speed": 10,
+          "size_min": 0.1,
+          "sync": false
+        }
+      },
+      "line_linked": {
+        "enable": true,
+        "distance": 150,
+        "color": "#32a852",
+        "opacity": 0.4,
+        "width": 1
+      },
+      "move": {
+        "enable": true,
+        "speed": 3,
+        "direction": "none",
+        "random": false,
+        "straight": false,
+        "out_mode": "out",
+        "bounce": false,
+        "attract": {
+          "enable": false,
+          "rotateX": 600,
+          "rotateY": 1200
+        }
       }
-    }
-  },
-  "retina_detect": true
-});
+    },
+    "interactivity": {
+      "detect_on": "canvas",
+      "events": {
+        "onhover": {
+          "enable": true,
+          "mode": "repulse"
+        },
+        "onclick": {
+          "enable": true,
+          "mode": "push",
+          "particles_nb": 10
+        },
+        "resize": true
+      },
+      "modes": {
+        "grab": {
+          "distance": 400,
+          "line_linked": {
+            "opacity": 1
+          }
+        },
+        "repulse": {
+          "distance": 200,
+          "duration": 0.4
+        },
+        "bubble": {
+          "distance": 10,
+          "size": 4,
+          "duration": 2,
+          "opacity": 8,
+          "speed": 3
+        },
+        "push": {
+          "particles_nb": 4
+        },
+        "remove": {
+          "particles_nb": 2
+        }
+      }
+    },
+    "retina_detect": true
+  });
 
 
+})
