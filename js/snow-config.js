6b5702ddacd17ca7f929354463698b5a50759fd5
Zak Schenck <zakschenck@Zaks-MBP-2.hsd1.fl.comcast.net>
2024-06-13 21:16:44 -0400

    Added vanta bg library

---
+++ js/snow-config.js
@@ -0,0 +1,7 @@
+VANTA.NET({
+  el: "#vanta-container",
+  color: 0xffffff, // Color of the dots
+  backgroundColor: 0x0, // Background color
+  points: 10, // Number of dots
+  spacing: 20 // Spacing between dots
+});
